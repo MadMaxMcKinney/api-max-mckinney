9e4e1589ae4ea40852e2fdcf594c816387bcd1a5
Max McKinney <maxgames101@gmail.com>
2022-02-28 22:14:59 -0600

    Testing stuff

--- pages/api/clusters.js
+++ pages/api/clusters.js
@@ -1,26 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import {clusterDB} from '../../db/db'
+//import {clusterDB} from '../../db/db'
 
 export default async function handler(req, res) {
 
-    await clusterDB.read()
+    //await clusterDB.read()
 
-    switch(req.method) {
+    // switch(req.method) {
 
-        case 'POST':
-            let newCluster = {severity: req.query.severity, comments: req.query.comments, date: req.query.date}
-            clusterDB.data.clusters.push(newCluster)
-            clusterDB.write()
-            res.status(200).json({status: "Success", cluster: newCluster})
-            break;
+    //     case 'POST':
+    //         let newCluster = {severity: req.query.severity, comments: req.query.comments, date: req.query.date}
+    //         clusterDB.data.clusters.push(newCluster)
+    //         clusterDB.write()
+    //         res.status(200).json({status: "Success", cluster: newCluster})
+    //         break;
 
-        case 'GET':
-            if(clusterDB.data.clusters) {
-                res.status(200).json(clusterDB.data.clusters)
-            } else {
-                res.status(404).json({status: "404"})
-            }
-            break;
+    //     case 'GET':
+    //         if(clusterDB.data.clusters) {
+    //             res.status(200).json(clusterDB.data.clusters)
+    //         } else {
+    //             res.status(404).json({status: "404"})
+    //         }
+    //         break;
 
-    }
+    // }
+
+    res.status(200).json({test: "yay"})
 }
