9a0560f4eba1ed75ba2e4cbe8abf5dc45bd0e470
Max McKinney <maxgames101@gmail.com>
2022-02-28 23:15:09 -0600

    More testing

--- db/db.js
+++ db/db.js
@@ -1,19 +1,14 @@
 const StormDB = require("stormdb");
 
-try {
-    // start db with "./db.stormdb" storage location
-    const engine = new StormDB.localFileEngine("./maxdb.json");
-    const db = new StormDB(engine);
+// start db with "./db.stormdb" storage location
+const engine = new StormDB.localFileEngine("./maxdb.json");
+const db = new StormDB(engine);
 
-    // Init defaults
-    db.default({
-        clusters: []
-    })
+// Init defaults
+db.default({
+    clusters: []
+})
 
-    db.save()
-} catch (error) {
-    console.log("Error in db.js")
-    console.log(error)
-}
+db.save()
 
 export default db
--- pages/api/clusters.js
+++ pages/api/clusters.js
@@ -1,40 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-try {
-    import db from '../../db/db'
-} catch (e) {
-    console.log(e)
-}
+import db from '../../db/db'
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
 
-    switch(req.method) {
+    // switch(req.method) {
 
-        case 'POST':
-            try {
-                let newCluster = {severity: req.query.severity, comments: req.query.comments, date: req.query.date}
+    //     case 'POST':
+    //         let newCluster = {severity: req.query.severity, comments: req.query.comments, date: req.query.date}
 
-                db.get("clusters").push(newCluster)
-                db.save()
-                
-                res.status(200).json({status: "Success", cluster: newCluster})
-            } catch (e) {
-                console.log("In post")
-                console.log(e)
-            }
-            break;
+    //         db.get("clusters").push(newCluster)
+    //         db.save()
+            
+    //         res.status(200).json({status: "Success", cluster: newCluster})
+    //         break;
 
-        case 'GET':
-            try {
-                if(db.get("clusters")) {
-                    res.status(200).json(db.get("clusters").value())
-                } else {
-                    res.status(404).json({status: "404"})
-                }
-            } catch (e) {
-                console.log("In post")
-                console.log(e)
-            }
-            break;
+    //     case 'GET':
+    //         if(db.get("clusters")) {
+    //             res.status(200).json(db.get("clusters").value())
+    //         } else {
+    //             res.status(404).json({status: "404"})
+    //         }
+    //         break;
 
-    }
+    // }
+
+    res.status(200).json({status: "200"})
 }
