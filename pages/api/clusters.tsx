e4b5baab468d4053386d38050988b30eb1fef3da
Max McKinney <maxgames101@gmail.com>
2022-03-02 15:19:59 -0600

    Changes to netlify.toml and initial typings for clusters.tsx

--- pages/api/clusters.tsx
+++ pages/api/clusters.tsx
@@ -1,16 +1,30 @@
-import { PrismaClient } from '@prisma/client'
+import { Cluster, PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next';
 
 const prisma = new PrismaClient()
 
-export default async function handler(req, res) {
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
 
     switch(req.method) {
+
+        case "POST":
+            let cluster:Cluster = await prisma.cluster.create({
+                data: {
+                    severity: req.query.severity,
+                    comments: req.query.comments,
+                    date: req.query.date
+                }
+            })
+            
+            res.status(200).json(JSON.stringify(cluster))
+
+            break;
 
         case 'GET':
             if(prisma.cluster) {
                 res.status(200).json(await prisma.cluster.findMany())
             } else {
-                res.status(404).json({status: "404"})
+                res.status(404).json({message: "No clusters"})
             }
             break;
 
