946545693582f325c634c27cfbd36abe5f5f8fab
Max McKinney <maxgames101@gmail.com>
2022-03-01 20:05:25 -0600

    Add Prisma and init db

--- pages/api/clusters.tsx
+++ pages/api/clusters.tsx
@@ -1,27 +1,18 @@
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 
 export default async function handler(req, res) {
 
-    //await clusterDB.read()
+    switch(req.method) {
 
-    // switch(req.method) {
+        case 'GET':
+            if(prisma.cluster) {
+                res.status(200).json(await prisma.cluster.findMany())
+            } else {
+                res.status(404).json({status: "404"})
+            }
+            break;
 
-    //     case 'POST':
-    //         let newCluster = {severity: req.query.severity, comments: req.query.comments, date: req.query.date}
-    //         clusterDB.data.clusters.push(newCluster)
-    //         clusterDB.write()
-    //         res.status(200).json({status: "Success", cluster: newCluster})
-    //         break;
-
-    //     case 'GET':
-    //         if(clusterDB.data.clusters) {
-    //             res.status(200).json(clusterDB.data.clusters)
-    //         } else {
-    //             res.status(404).json({status: "404"})
-    //         }
-    //         break;
-
-    // }
-
-    res.status(200).json({test: "yay"})
+    }
 }
---
+++ prisma/migrations/20220302015509_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Cluster" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "severity" TEXT NOT NULL,
+    "comments" TEXT NOT NULL,
+    "date" TEXT NOT NULL
+);
